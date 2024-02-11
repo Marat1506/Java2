549d408a9fa5f69e0dfff318e2e8531c689ae46d
Marat <maratmirzabalaev@gmail.com>
2024-02-12 02:27:43 +0300

    secondCommit

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,9 @@ public class Main {
 
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
+            if(i == 2){
+                System.out.println("gg");
+            }
 
             // Press Shift+F9 to start debugging your code. We have set one breakpoint
             // for you, but you can always add more by pressing Ctrl+F8.
